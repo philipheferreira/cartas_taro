969dcc5405d609f79cf55b2629adc51cad7fcfac
philipheferreira <philipheferreira@gmail.com>
2023-09-21 17:08:08 -0300

    update: flip function

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,21 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <q-img src="../image/back_card.jpg" style="height: 450px; width: 300px;" label="Flip Menu">
+      <q-menu transition-show="flip-right" transition-hide="flip-left">
+        <q-list style="min-width: 100px">
+          <q-item clickable>
+            <q-item-section>Having fun</q-item-section>
+          </q-item>
+          <q-item clickable>
+            <q-item-section>Crazy for transitions</q-item-section>
+          </q-item>
+          <q-separator />
+          <q-item clickable>
+            <q-item-section>Mind blown</q-item-section>
+          </q-item>
+        </q-list>
+      </q-menu>
+    </q-img>
   </q-page>
 </template>
 
